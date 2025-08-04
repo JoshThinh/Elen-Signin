a2db452a9b05b98a19c3daa4943a5fcaeaff3881
JoshThinh <joshua.thinh@gmail.com>
2025-08-04 11:50:17 -0700

    allowed for local host

--- config.js
+++ config.js
@@ -1,2 +1,6 @@
 window.API_URL = "http://localhost:5000";
 window.API_URL_DEPLOYED = "https://web-production-6b1da.up.railway.app"; 
+
+window.ApiUrlLink = location.hostname === "localhost"
+? window.API_URL
+: window.API_URL_DEPLOYED;
